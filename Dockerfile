26c4dde12df0551bad8ab8d50293759a5f7d9a33
Arya P K <aryapk1913@gmail.com>
2024-06-30 20:10:10 +0530

    Dockerised and pushed to Docker hub

---
+++ Dockerfile
@@ -0,0 +1,8 @@
+FROM golang:1.21.4-alpine3.18 AS build-stage
+WORKDIR /app
+COPY ./ /app
+RUN mkdir -p /app/build
+RUN go mod download
+RUN go build -v -o /app/build/api ./cmd/api
+EXPOSE 8080
+CMD ["/app/build/api"]
--- pkg/usecase/user_test.go
+++ pkg/usecase/user_test.go
@@ -15,13 +15,12 @@ func TestFindUser_Success(t *testing.T) {
 	ctrl := gomock.NewController(t)
 	defer ctrl.Finish()
 
-	// Create a mock repository
 	mockRepo := mock.NewMockUserRepository(ctrl)
 
 	// Create a user use case with the mock repository
 	userUseCase := usecase.NewUserUseCase(mockRepo)
 
-	// Define a test user
+	
 	testUser := domain.User{
 		ID: 1, UserName: "Ajin", FirstName: "Ajin", LastName: "A", Age: 22, Email: "ajin@gmail.com", Phone: "9074386600", Password: "ajin123", BlockStatus: false,
 	}
@@ -30,12 +29,10 @@ func TestFindUser_Success(t *testing.T) {
 	mockRepo.EXPECT().
 		FindUser(gomock.Any(), testUser).
 		Return(testUser, nil).
-		Times(1) // Expect the FindUser method to be called once with the test user
-
+		Times(1) 
 	// Call the FindUser method
 	found, err := userUseCase.FindUser(context.Background(), testUser)
 
-	// Check if user is found and error is nil
 	assert.True(t, found, "expected user to be found")
 	assert.NoError(t, err, "expected no error")
 }
